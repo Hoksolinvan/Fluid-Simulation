7d533d76a38344702c0bab26ead8b2ca9497c9b6
Hoksolinvan Chhun <158796823+Hoksolinvan@users.noreply.github.com>
2026-06-10 17:01:43 -0700

    Refactor main function and update border initializer

--- main.cpp
+++ main.cpp
@@ -2,66 +2,56 @@
 #include <cstring>
 
 
-#define IX(i,j) ((i)+(N+2)*j)
+#define IX(i,j) ((i)+(N+2)*(j))
 
 const int N = 16;
 char grid[(N+2)*(N+2)];
 float dens[(N+2)*(N+2)];
+float dens_prev[(N+2)*(N+2)];
 float source[(N+2)*(N+2)];
+float dt =1;
 
 void printout();
-void border_initializer();
+void border_initializer(float* dens);
 void add_source(float* x, float* s, float dt);
+void diffuse(float* x=dens, float* x0=dens_prev, float diff=0.001f, float dt=1);
+
 
 int main(){
-
-   memset(grid,'.',sizeof(grid));
-    float source[(N+2)*(N+2)];
-    memset(source,0,sizeof(source));
+    memset(dens, 0, sizeof(dens));
+    memset(dens_prev, 0, sizeof(dens_prev));
     
-    source[IX(8,8)]= 5.0f;
+    dens_prev[IX(8,8)] = 100.0f;
     
-    add_source(dens,source,1);
-    
-    for(int i =0; i<(N+2)*(N+2);i++){
-        printf("%.2f ",dens[i]);
-        
-    }
-    border_initializer();
-    
+   for(int step = 0; step < 10; step++){
+    diffuse(dens, dens_prev, 0.002f, 1.0f);
+    printout();
+    std::cout << "--- step " << step << " ---" << std::endl;
+    memcpy(dens_prev, dens, sizeof(dens));
+}
+    return 0;
 
-    grid[IX(9,6)]='#';
-    grid[IX(9,7)]='#';
-    grid[IX(9,8)]='#';
-    grid[IX(10,7)]='#';
-    grid[IX(8,7)]='#';
-   
-
-  //printout();
-
-  
-
-return 0;
 }
 
 
 
-void border_initializer(){
+void border_initializer(float* dens){
     
     for(int i=0;i<N+2;i++){
-        grid[IX(i,0)]='*';
+        dens[IX(i,0)]=-1;
     }
     
     for(int i=0;i<N+2;i++){
-        grid[IX(0,i)]='*';
+        dens[IX(0,i)]=-1;
     }
     
     for(int i=0;i<N+2;i++){
-        grid[IX(N+1,i)]='*';
+        dens[IX(N+1,i)]=-1;
     }
     
+    
     for(int i=0;i<N+2;i++){
-        grid[IX(i,N+1)]='*';
+        dens[IX(i,N+1)]=-1;
     }
     
     
@@ -73,7 +63,18 @@ void printout(){
     
     for(int j=0; j<(N+2);j++){
         for(int i=0; i<(N+2);i++){
-            std::cout << grid[IX(i,j)]<< " ";
+            
+            if(dens[IX(i,j)]>1.0f){
+                std::cout << "# " ; 
+            }
+            else if(dens[IX(i,j)]==0){
+                std::cout << ". ";
+            }
+            else{
+                std::cout << ". ";
+                //std::cout << "* ";
+            }
+            
         }
         std::cout << std::endl;
     }
@@ -91,3 +92,23 @@ void add_source(float* x, float* s, float dt){
     
     return;
 }
+
+
+void diffuse(float* x, float* x0, float diff, float dt){
+    
+    
+    float a = dt * diff * N * N;
+    
+    
+    for(int k =0; k<20;k++){
+    for(int j=1; j<N+1;j++){
+        for(int i=1; i<N+1;i++){
+    
+            x[IX(i,j)]=(x0[IX(i,j)]+a*(x[IX(i-1,j)]+x[IX(i+1,j)]+x[IX(i,j-1)]+x[IX(i,j+1)])) / (1+4*a);
+        }
+    }
+    }
+    
+    return;
+}
+
